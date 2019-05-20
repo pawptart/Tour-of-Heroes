d3c0e97c463b9444f4c8a62857d3854e8e5ee64d
unknown <tyler.b.porter@gmail.com>
2019-05-20 14:08:30 -0500

    Added message logging

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
   selectedHero: Hero;
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
